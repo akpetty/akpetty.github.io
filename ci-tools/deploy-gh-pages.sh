6511df5f1e0781f16f7c3a7508691abc79cc37a6
Alek <alekpetty@gmail.com>
2016-11-15 11:46:42 -0500

    add

--- ci-tools/deploy-gh-pages.sh
+++ ci-tools/deploy-gh-pages.sh
@@ -2,7 +2,8 @@
 # Based on
 # http://sleepycoders.blogspot.com.au/2013/03/sharing-travis-ci-generated-files.html
 # and https://github.com/richfitz/wood
-echo -e "Preparing to copy generated files to leouieda.com"
+echo -e "Preparing to copy generated files to alekpetty.com"
+echo "$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     echo -e "Starting to update website\n"
     cp -R _build $HOME/keep
@@ -14,13 +15,13 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
     echo -e "Cloning project"
     # Clone the project, using the secret token. Uses /dev/null to avoid leaking decrypted key
     git clone --quiet --branch=master --single-branch https://${GH_TOKEN}@github.com/akpetty/akpetty.github.com.git leouieda.com > /dev/null
-    cd leouieda.com
+    cd alek.com
     # Move the old branch out of the way and create a new one:
     git branch -m master-old
     git checkout --orphan master
     # Delete all the files and replace with our good set
     git rm -rf .
-    cp -Rf $HOME/keep/. $HOME/leouieda.com
+    cp -Rf $HOME/keep/. $HOME/akpetty.com
     # add, commit and push files
     git add -f .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to akpetty.github.com"
