9e7e3ee3eefab679440e63ed56a48f35cf9439af
akpetty <alekpetty@gmail.com>
2026-02-20 07:32:00 -0500

    update

--- commitUpdates.sh
+++ commitUpdates.sh
@@ -6,6 +6,11 @@ make
 echo -e "build"
 cp -R _build/* ../akpetty.github.io
 
+# Copy .gitignore to destination repo
+if [ -f .gitignore ]; then
+    cp .gitignore ../akpetty.github.io/
+fi
+
 echo -e "commit to Git"
 cd ../akpetty.github.io
 
